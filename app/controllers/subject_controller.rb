fafe0427f531fac7f041719959eadd6d3ccc281a
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2010-07-25 17:14:38 +0900

    (tmp) Created 'subject/edit' preliminarily
    app/controllers/subject_controller.rb
    - Extracted helper prepare_for_list() from list().
    - Added edit().
    app/models/category.rb
    - Added has_many :subjects.
    app/views/subject/_list.html.erb
    - Display form contents in an appropriate row if @subject.
    app/views/subject/edit.html.erb
    - Newly created.

--- app/controllers/subject_controller.rb
+++ app/controllers/subject_controller.rb
@@ -1,10 +1,23 @@
 class SubjectController < ApplicationController
 
   def list
-    @subjects = Subject.find(:all)
-    @column_names  = Subject.columns.map(&:name)
+    prepare_for_list
 
     @page_title = "お題のリスト"
+    @page_title_size = 3
+  end
+
+    def prepare_for_list
+      @subjects = Subject.find(:all)
+      @column_names  = Subject.columns.map(&:name)
+    end
+    private :prepare_for_list
+
+  def edit
+    prepare_for_list
+    @subject = Subject.find(params[:id])
+
+    @page_title = "お題の編集"
     @page_title_size = 3
   end
 end
