9c6dd92e225065588dd329ceac85ce26791897cf
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2010-07-25 22:04:21 +0900

    Implemented 'subject/edit->update'
    app/controllers/subject_controller.rb
    - Extracted prepare_for_render_edit() from edit().
    - Added update().
    app/views/subject/_list.html.erb
    - Fixed :action in the form_for to local 'action'.
    app/views/subject/edit.html.erb
    - Fixed local :action in render '_list' to 'update'.

--- app/controllers/subject_controller.rb
+++ app/controllers/subject_controller.rb
@@ -17,7 +17,24 @@ class SubjectController < ApplicationController
     prepare_for_list
     @subject = Subject.find(params[:id])
 
-    @page_title = "お題の編集"
-    @page_title_size = 3
+    prepare_for_render_edit
+  end
+
+    def prepare_for_render_edit
+      @page_title = "お題「#{@subject.name_zen}」の編集"
+      @page_title_size = 3
+    end
+    private :prepare_for_render_edit
+
+  def update
+    h_subject = params[:subject]
+    @subject = Subject.find(h_subject[:id])
+    if @subject.update_attributes(h_subject)
+      redirect_to :action => 'list'
+    else
+      prepare_for_list
+      prepare_for_render_edit
+      render :action => 'edit'
+    end
   end
 end
