046f4723ce99bbb7f3b98f2e320bb2aacd13b6f2
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2010-07-29 22:49:41 +0900

    Justify 'Total Count' in Subject count table
    app/helpers/application_helper.rb
    - Added justify().
    app/views/subject/_counts.html.erb
    - Justify 'Total Count'.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def justify(str, num_spaces_between)
+    return str.split(//).join('&nbsp;' * num_spaces_between)
+  end
 end
