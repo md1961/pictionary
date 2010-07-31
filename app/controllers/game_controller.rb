f4f84d69dab533f0f8d73d627e1612d43be07841
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2010-07-31 19:27:27 +0900

    Added start(), turn() and pick_category() to GameController
    app/controllers/game_controller.rb
    - Added start(), turn() and pick_category().
    app/views/game/pick_category.html.erb
    app/views/game/turn.html.erb
    - Newly created.
    public/stylesheets/style.css
    - Added style for '.game_display_base'.

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -4,4 +4,21 @@ class GameController < ApplicationController
     @page_title = "トップページ"
   end
 
+  def start
+    redirect_to :action => 'turn'
+  end
+
+  def turn
+    @page_title = "ジャンルの決定"
+  end
+
+  def pick_category
+    categories = Category.all
+    @category = categories[rand(categories.size)]
+
+    @page_title = "ジャンルが決定されました"
+  end
+
+  def show_subject
+  end
 end
