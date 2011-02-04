d3c659c8ecf6fbd08a5beee82da8c291f191069f
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2011-02-05 08:18:00 +0900

    Converted to RESTful routes (up to 'games#index')
    app/views/games/index.html.erb
    app/views/layouts/application.html.erb
    - Modified to use RESTful paths.
    config/routes.rb
    - Added named route 'game' and 'start_game'.
    - Added resources route :subjects.

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,12 @@ ActionController::Routing::Routes.draw do |map|
   # Sample of named route:
   #   map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
   # This route can be invoked with purchase_url(:id => product.id)
+  map.game       'game'      , :controller => 'games', :action => 'index'
+  map.start_game 'game/start', :controller => 'games', :action => 'start'
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   map.resources :products
+  map.resources :subjects
 
   # Sample resource route with options:
   #   map.resources :products, :member => { :short => :get, :toggle => :post }, :collection => { :sold => :get }
