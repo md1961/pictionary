8550b45ae07b04ea53251f8854f012908c07618a
Naoyuki Kumagai <kumagai@fergus>
2011-02-06 12:44:49 +0900

    [Temporary] First import with Rails 3.0 empty project files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pictionary::Application.config.secret_token = 'd60acfd45991d53777137d175126405f2262f0196ec6f8712b71e27d055dcafd6b26c30a1c0b4327b4bf0ca3f9312086553415ced4cb9edb7a163089149d14c8'
