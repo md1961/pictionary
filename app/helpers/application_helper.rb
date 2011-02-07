b9dab5dbc3373e054c2be11955e9dfbe8947b895
Naoyuki Kumagai <kumagai@fergus>
2011-02-07 20:51:19 +0900

    Added ApplicationHelper#nbsp() to avoid HTML escape
    app/helpers/application_helper.rb
    - Added nbsp().
    app/views/subjects/_links_in_list.html.erb
    app/views/subjects/_list.html.erb
    - Modified to use ApplicationHelper#nbsp().

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
 
+  def nbsp(num=1)
+    return ('&nbsp' * num).html_safe
+  end
+
   def justify(str, num_spaces_between)
     return str.split(//).join('&nbsp;' * num_spaces_between)
   end
